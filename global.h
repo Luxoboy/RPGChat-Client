50bea7f93db9af2cf82e89ddb64d0ca76cf7d577
Anthony Correia <anthony.correia71@gmail.com>
2015-04-03 15:50:01 +0200

    Init.

---
+++ global.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   global.h
+ * Author: Anthony Correia <anthony.correia71@gmail.com>
+ *
+ * Created on 3 avril 2015, 15:20
+ */
+
+#ifndef GLOBAL_H
+#define	GLOBAL_H
+
+extern void readMessage(char*);
+
+#endif	/* GLOBAL_H */
+
---
+++ main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Anthony Correia <anthony.correia71@gmail.com>
+ *
+ * Created on 3 avril 2015, 15:42
+ */
+
+#include <cstdlib>
+
+#include "global.h"
+#include "network.h"
+
+#include "json/json.h"
+
+using namespace std;
+using namespace Json;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    
+    return 0;
+}
+
+void readMessage(char* msg)
+{
+    
+}
